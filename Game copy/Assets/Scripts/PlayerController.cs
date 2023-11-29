8a6a4f07f562b8e877429d7e103447e42b9beb74
jf0ley <b00151368@mytudublin.ie>
2023-11-29 19:23:13 +0000

    Basic Sound Effects

--- Game copy/Assets/Scripts/PlayerController.cs
+++ Game copy/Assets/Scripts/PlayerController.cs
@@ -12,7 +12,19 @@ public class PlayerController : MonoBehaviour
     public float floorWidth = 20.0f;
     public float laneWidth;
     private Animator playerAnim;
+    private AudioSource audioSource;
+    private AudioSource runningAudioSource;
+    private AudioSource coinAudioSource;
+    private AudioSource powerupAudioSource;
+    private AudioSource deathAudioSource;
+    public AudioClip runningAudio;
+    public AudioClip coinAudio;
+    public AudioClip powerupAudio;
+    public AudioClip deathAudio;
     public ParticleSystem explosionParticle;
+    public ParticleSystem crouchParticle; // Assign in the inspector
+    public ParticleSystem uncrouchParticle; // Assign in the inspector
+    public ParticleSystem runEffect; // Assign in the inspector
     private float currentLane = 1;
     private Rigidbody playerRb;
     public float jumpForce;
@@ -25,92 +37,154 @@ public class PlayerController : MonoBehaviour
     private bool isInvincible = false;
     private float powerupDuration = 5.0f;
     private float powerupTimer;
-    
 
-
-    private int doubleJumpCount = 1;  // Set to 1 for initial double jump ability
-    private int currentDoubleJumps = 0;  // Keep track of the number of double jumps used
-
-    // Add these variables
+    // Double Jump variables
     private bool doubleJumpAvailable = false;
     private float doubleJumpDuration = 5.0f;
     private float doubleJumpTimer = 0.0f;
-
+    private int doubleJumpCount = 1;
+    private int currentDoubleJumps = 0;
+    // Crouch variables
     private bool isCrouching = false;
     private float originalHeight;
 
     void Start()
     {
+        // Initialize components and variables
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
         playerAnim = GetComponent<Animator>();
 
+        // Add the following lines to create and configure AudioSource components
+        audioSource = gameObject.AddComponent<AudioSource>();
+        runningAudioSource = gameObject.AddComponent<AudioSource>();
+        coinAudioSource = gameObject.AddComponent<AudioSource>();
+        powerupAudioSource = gameObject.AddComponent<AudioSource>();
+        deathAudioSource = gameObject.AddComponent<AudioSource>();
+
+        // Configure AudioSource settings
+        ConfigureAudioSource(audioSource, null);
+        ConfigureAudioSource(runningAudioSource, runningAudio);
+        ConfigureAudioSource(coinAudioSource, coinAudio);
+        ConfigureAudioSource(powerupAudioSource, powerupAudio);
+        ConfigureAudioSource(deathAudioSource, deathAudio);
+
         laneWidth = floorWidth / numberOfLanes;
         originalHeight = transform.localScale.y;
     }
-    
 
     void Update()
     {
+        // Handle player movement
+        HandleMovementInput();
+
+        // Handle jumping
+        HandleJumping();
+
+        // Handle crouching
+        HandleCrouching();
+
+        // Handle dodge animation
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            StartCoroutine(PerformDodge());
+        }
+
+        // Handle power-up timers
+        HandlePowerupTimers();
+
+        // Update particle effects
+        UpdateParticleEffects();
+    }
+
+    void HandleMovementInput()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
 
+        // Move left
         if (Input.GetKeyDown(KeyCode.LeftArrow) && currentLane > 0 && !isCrouching)
         {
             currentLane--;
         }
 
+        // Move right
         if (Input.GetKeyDown(KeyCode.RightArrow) && currentLane < numberOfLanes - 1 && !isCrouching)
         {
             currentLane++;
         }
 
+        // Move towards the target lane
         float targetX = currentLane * laneWidth - floorWidth / 2.0f + laneWidth / 2.0f;
         transform.position = Vector3.Lerp(transform.position, new Vector3(targetX, transform.position.y, transform.position.z), Time.deltaTime * speed);
 
+        // Clamp player position within the bounds
         float clampedX = Mathf.Clamp(transform.position.x, -floorWidth / 2.0f + laneWidth / 2.0f, floorWidth / 2.0f - laneWidth / 2.0f);
         transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
 
+        // Move forward based on horizontal input
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
+        // Play running audio when moving
+        if (Mathf.Abs(horizontalInput) > 0.1f && isOnGround && !runningAudioSource.isPlaying)
+        {
+            runningAudioSource.Play();
+        }
+        else if (Mathf.Abs(horizontalInput) < 0.1f)
+        {
+            // Stop running audio when not moving
+            runningAudioSource.Stop();
+        }
+    }
+
+    void HandleJumping()
+    {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (isOnGround || (doubleJumpAvailable && doubleJumpTimer > 0))
             {
-                playerRb.velocity = new Vector3(playerRb.velocity.x, 0, playerRb.velocity.z);
-                playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-                playerAnim.SetBool("Jump_b", true);
+                // Perform jump
+                PerformJump();
 
+                // Disable double jump after the first jump
                 if (!isOnGround)
                 {
                     doubleJumpAvailable = false;
                     doubleJumpTimer = 0.0f;
                     texts.doubleJumpPowerupText.gameObject.SetActive(false);
-                    playerAnim.SetBool("Jump_b", false);
                 }
 
                 isOnGround = false;
-                
             }
-            
         }
 
-        if (Input.GetKeyUp(KeyCode.Space)) {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            // Set the "Jump_b" parameter to false in the animator
             playerAnim.SetBool("Jump_b", false);
         }
-        
+    }
 
-        // Check for crouch input
+    void PerformJump()
+    {
+        // Reset vertical velocity and apply jump force
+        playerRb.velocity = new Vector3(playerRb.velocity.x, 0, playerRb.velocity.z);
+        playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        playerAnim.SetBool("Jump_b", true);
+    }
+
+    void HandleCrouching()
+    {
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
+            // Crouch if on the ground
             if (isOnGround)
             {
-                // Crouch if on the ground
                 isCrouching = true;
                 Crouch();
             }
+            // Descend more quickly when crouching while in the air
             else if (!isOnGround && !isCrouching)
             {
-                // Descend more quickly when crouching while in the air
                 playerRb.velocity = new Vector3(playerRb.velocity.x, -jumpForce, playerRb.velocity.z);
                 isCrouching = true;
                 Crouch();
@@ -123,24 +197,44 @@ public class PlayerController : MonoBehaviour
             Uncrouch();
         }
 
-        if (isCrouching)
-        {
-            // Additional actions when continuously crouching
-            // Example: Decrease speed when continuously crouching
-            speed /= 2.0f;
-        }
-        else
-        {
-            // Restore speed when not crouching
-            speed = 10.0f;
-        }
+        // Adjust speed based on crouching state
+        AdjustSpeedBasedOnCrouch();
+    }
 
+    void Crouch()
+    {
+        // Adjust the player's scale to crouch height
+        transform.localScale = new Vector3(transform.localScale.x, originalHeight / 2.0f, transform.localScale.z);
+
+        // Play crouch particle effect
+        crouchParticle.Play();
+    }
+
+    void Uncrouch()
+    {
+        // Restore the player's original scale
+        transform.localScale = new Vector3(transform.localScale.x, originalHeight, transform.localScale.z);
+
+        // Play uncrouch particle effect
+        uncrouchParticle.Play();
+    }
+
+    void AdjustSpeedBasedOnCrouch()
+    {
+        // Adjust speed when continuously crouching
+        speed = isCrouching ? 5.0f : 10.0f;
+    }
+
+    void HandlePowerupTimers()
+    {
+        // Handle invincibility power-up timer
         if (isInvincible)
         {
             powerupTimer -= Time.deltaTime;
             powerupTimer = Mathf.Max(0, powerupTimer);
 
             texts.InvincibilityText(powerupTimer);
+
             if (powerupTimer <= 0)
             {
                 isInvincible = false;
@@ -149,6 +243,7 @@ public class PlayerController : MonoBehaviour
             }
         }
 
+        // Handle double jump power-up timer
         if (!isOnGround && doubleJumpTimer > 0)
         {
             doubleJumpTimer -= Time.deltaTime;
@@ -161,113 +256,198 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
-{
-    if (collision.gameObject.CompareTag("Ground"))
+    void UpdateParticleEffects()
     {
-        isOnGround = true;
-        currentDoubleJumps = 0;  // Reset double jump count when grounded
-        if (isCrouching)
+        // Update runEffect particle system
+        if (!isCrouching && !isOnGround && !gameOver)
         {
-            // Automatically uncrouch when landing on the ground
-            isCrouching = false;
-            Uncrouch();
+            if (!runEffect.isPlaying)
+            {
+                runEffect.Play();
+            }
+        }
+        else
+        {
+            runEffect.Stop();
         }
     }
 
-    if (collision.gameObject.CompareTag("Enemy"))
+    void OnCollisionEnter(Collision collision)
+    {
+        // Handle collisions with ground, enemies, coins, and power-ups
+        HandleCollisions(collision);
+    }
+
+    void HandleCollisions(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            // Player is on the ground
+            isOnGround = true;
+
+            // Reset double jump count when grounded
+            currentDoubleJumps = 0;
+
+            // Automatically uncrouch when landing on the ground
+            if (isCrouching)
+            {
+                isCrouching = false;
+                Uncrouch();
+            }
+        }
+        else if (collision.gameObject.CompareTag("Enemy"))
+        {
+            // Handle collision with enemy
+            HandleEnemyCollision();
+        }
+        else if (collision.gameObject.CompareTag("Coin"))
+        {
+            // Handle collision with coin
+            CollectCoin(collision.gameObject);
+
+            // Play coin audio
+            coinAudioSource.Play();
+        }
+        else if (collision.gameObject.CompareTag("PowerUp"))
+        {
+            // Handle collision with invincibility power-up
+            CollectPowerup(collision.gameObject);
+
+            // Play power-up audio
+            powerupAudioSource.Play();
+        }
+        else if (collision.gameObject.CompareTag("Powerup2"))
+        {
+            // Handle collision with double jump power-up
+            CollectDoubleJump(collision.gameObject);
+
+            // Play power-up audio
+            powerupAudioSource.Play();
+        }
+    }
+
+    void HandleEnemyCollision()
     {
         if (!isInvincible && !gameOver)
         {
+            // Player is not invincible and the game is not over
+            // Handle game over scenario
+
             gameOver = true;
             texts.gameoverText.gameObject.SetActive(true);
             restartButton.gameObject.SetActive(true);
             returnMenuButton.gameObject.SetActive(true);
-                playerAnim.SetBool("Death_b", true);
-                playerAnim.SetInteger("DeathType_int", 1);
-                explosionParticle.Play();
-                moveForwardScript.fire.Stop();
-                
-                
+
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+
+            // Play explosion particle effect
+            explosionParticle.Play();
+
+            // Play death audio
+            deathAudioSource.Play();
+
+            // Stop the forward movement
+            moveForwardScript.fire.Stop();
         }
     }
 
-    if (collision.gameObject.CompareTag("Coin"))
+    void CollectCoin(GameObject coin)
     {
-        CollectCoin(collision.gameObject);
-    }
-    else if (collision.gameObject.CompareTag("PowerUp"))
-    {
-        CollectPowerup(collision.gameObject);
-    }
-    else if (collision.gameObject.CompareTag("Powerup2"))
-    {
-        CollectDoubleJump(collision.gameObject);
-    }
-}
-
-    public void RestartGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-
-    public void ReturnMenu()
-    {
-        SceneManager.LoadSceneAsync(0);
-    }
-
-    private void CollectCoin(GameObject Coin)
-    {
+        // Increase score and destroy the coin
         texts.score += 100;
-        Destroy(Coin);
+        Destroy(coin);
     }
 
-    private void CollectPowerup(GameObject PowerUp)
+    void CollectPowerup(GameObject powerUp)
     {
+        // Activate invincibility power-up
         StartCoroutine(ActivateInvincibility());
+
+        // Set the power-up timer
         powerupTimer = powerupDuration;
+
+        // Update UI for invincibility
         texts.InvincibilityText(powerupTimer);
-        Destroy(PowerUp);
+
+        // Destroy the power-up object
+        Destroy(powerUp);
     }
 
     IEnumerator ActivateInvincibility()
     {
+        // Activate invincibility for a duration
         isInvincible = true;
         yield return new WaitForSeconds(5.0f);
         isInvincible = false;
     }
 
-    private void CollectDoubleJump(GameObject PowerUp)
+    void CollectDoubleJump(GameObject powerUp)
     {
+        // Activate double jump power-up
         StartCoroutine(ActivateDoubleJump());
-        Destroy(PowerUp);
+
+        // Destroy the power-up object
+        Destroy(powerUp);
+
+        // Update UI for double jump power-up
         texts.DoubleJumpPowerupText(doubleJumpDuration);
     }
 
     IEnumerator ActivateDoubleJump()
     {
+        // Activate double jump for a duration
         doubleJumpAvailable = true;
         doubleJumpTimer = doubleJumpDuration;
         yield return new WaitForSeconds(doubleJumpDuration);
         doubleJumpAvailable = false;
     }
 
-    // Add these functions for crouch functionality
-    private void Crouch()
+    // Coroutine to animate the collected coin (if needed)
+    IEnumerator AnimateCoin(GameObject coin)
     {
-        // Adjust the player's scale to crouch height
-        transform.localScale = new Vector3(transform.localScale.x, originalHeight / 2.0f, transform.localScale.z);
+        float duration = 0.5f;
+        float elapsed = 0f;
+        Vector3 originalScale = coin.transform.localScale;
 
-        // Additional actions when crouching
-        Debug.Log("Player is crouching!");
+        while (elapsed < duration)
+        {
+            coin.transform.localScale = Vector3.Lerp(originalScale, Vector3.zero, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        Destroy(coin);
     }
 
-    private void Uncrouch()
+    IEnumerator PerformDodge()
     {
-        // Restore the player's original scale
-        transform.localScale = new Vector3(transform.localScale.x, originalHeight, transform.localScale.z);
+        // Play dodge animation
+        playerAnim.SetTrigger("Dodge_trig");
 
-        // Additional actions when uncrouching
-        Debug.Log("Player is uncrouching!");
+        // Wait for the animation duration
+        yield return new WaitForSeconds(playerAnim.GetCurrentAnimatorStateInfo(0).length);
+
+        // End dodge animation
+        playerAnim.ResetTrigger("Dodge_trig");
+    }
+
+    public void RestartGame()
+    {
+        // Restart the current scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ReturnMenu()
+    {
+        // Return to the main menu
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    // Helper method to configure AudioSource settings
+    private void ConfigureAudioSource(AudioSource source, AudioClip clip)
+    {
+        source.clip = clip;
+        // Add any other AudioSource configuration settings here
     }
 }
