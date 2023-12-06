98bbd17b0aa845adefaf7589dc3667bece1f039c
erykglowacki1 <B00153656@mytudublin.ie>
2023-12-06 10:53:00 +0000

    more ui tweaks

---
+++ Game copy/Assets/Scripts/LinkToGithub.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkToGithub : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     public void OpenUrl()
+    {
+        Application.OpenURL("https://github.com/erykglowacki1/BetaVersionProject");
+    }
+}
